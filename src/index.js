8d0a535cf9bf62a36750a373e50497a87c0eb8fb
farshidb <farshidb2011@gmail.com>
2020-10-26 11:44:50 +0330

    befor use html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import './style.css';
-import farshid from './26037.svg';
+import 'Components/style.css';
+import farshid from 'Components/26037.svg';
+import printMe from './print';
 
 function component(){
     const element = document.createElement('div');
-    element.innerHTML = "Hello webpack";
-    element.classList.add('hello');
-    
+    const btn = document.createElement('button');
     const imgFarshid = new Image(500);
+
+    element.innerHTML = "سلام به وب پک.";
+    btn.innerHTML = 'Click me and check the console';
+
+    element.classList.add('hello');
+    btn.onclick = printMe;
+
+    
     imgFarshid.classList.add('imgFarshid');
     imgFarshid.src = farshid;
 
     element.appendChild(imgFarshid);
+    element.appendChild(btn);
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,20 @@ const path = require('path');
 
 module.exports ={
 
-    entry : './src/index.js',
+    entry : {
+        app: './src/index.js',
+        print: './src/print.js',
+    },
+
     output : {
-        filename : 'bundle.js',
+        filename : '[name].bundle.js',
         path : path.resolve(__dirname,'dist')
+    },
+
+    resolve :{
+        alias:{
+            Components : path.resolve(__dirname,'public/components')
+        },
     },
 
     module:{
@@ -20,6 +30,12 @@ module.exports ={
             {
                 test : /\.(png|jpg|svg|gif)$/,
                 use:[
+                    'file-loader'
+                ],
+            },
+            {
+                test : /\.(woff|woff2|eot|ttf|otf)$/,
+                use :[
                     'file-loader'
                 ],
             },
